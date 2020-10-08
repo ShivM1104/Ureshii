72d4aa253f9c7deab7a5e2cada05c84f1cc7a9c1
Shiv Mehta <shivmehta@Shivs-MacBook-Pro-2.local>
2020-10-08 18:05:30 +0530

    Second commit

---
+++ screens/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { Block, theme } from 'galio-framework';
+
+import { Card } from '../components';
+import articles from '../constants/articles';
+const { width } = Dimensions.get('screen');
+
+class Home extends React.Component {
+  render() {
+    return <Block />;
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default Home;
